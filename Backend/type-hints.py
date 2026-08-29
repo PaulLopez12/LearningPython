23df03469d55b4153f699cc02d382a509151909a
PaulLopez12 <paul.lopez.b@uni.pe>
2026-08-29 16:31:39 -0500

    added type-hints

---
+++ Backend/type-hints.py
@@ -0,0 +1,17 @@
+### Type Hints ###
+
+my_string_variable = "My string variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 2
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_typed_variable: int = "My typed string variable" # Lenguaje de tipado suave
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+my_typed_variable = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
