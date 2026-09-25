0e7f9fc3d1b3d710a53e3f5d2b5aa218158429ca
PaulLopez12 <67396684+PaulLopez12@users.noreply.github.com>
2026-09-25 17:12:51 -0500

    CORS

--- Frontend/link_bio/rxconfig.py
+++ Frontend/link_bio/rxconfig.py
@@ -2,6 +2,10 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
+    cors_allowed_origins=[
+        "http://localhost:3000/",
+        "https://moure.dev"
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
