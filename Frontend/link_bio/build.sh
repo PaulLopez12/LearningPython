33c59b029e3956734703c2f198bbade870f53bdd
PaulLopez12 <67396684+PaulLopez12@users.noreply.github.com>
2026-09-25 16:56:08 -0500

    Dockerfile modificado

--- Frontend/link_bio/build.sh
+++ Frontend/link_bio/build.sh
@@ -1,9 +1,9 @@
-.venv/Scripts/activate
+. .venv/Scripts/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-reflex export --frontend-only
+REFLEX_SSR=false REFLEX_API_URL=https://primera-web-mouredev.up.railway.app/ reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
