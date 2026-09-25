131818248fb603aa5c179042ea4ba5482f153b64
PaulLopez12 <67396684+PaulLopez12@users.noreply.github.com>
2026-09-25 17:32:04 -0500

    Construcción del frontend en remoto

--- Frontend/link_bio/remote_build.sh
+++ Frontend/link_bio/remote_build.sh
@@ -1,3 +1,5 @@
+cd link_bio
+python -m venv .venv
 . .venv/Scripts/activate
 pip install --upgrade pip
 pip install -r requirements.txt
--- Frontend/link_bio/rxconfig.py
+++ Frontend/link_bio/rxconfig.py
@@ -3,7 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="link_bio",
     cors_allowed_origins=[
-        "http://localhost:3000/",
+        "http://localhost:3000",
         "https://moure.dev"
     ],
     plugins=[
