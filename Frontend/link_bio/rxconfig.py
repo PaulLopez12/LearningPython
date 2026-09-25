5780d713caafb8493f9564a93d240f5bc9cdd314
PaulLopez12 <67396684+PaulLopez12@users.noreply.github.com>
2026-09-25 18:06:56 -0500

    agregada página vercel a rx.config

--- Frontend/link_bio/rxconfig.py
+++ Frontend/link_bio/rxconfig.py
@@ -4,7 +4,8 @@ config = rx.Config(
     app_name="link_bio",
     cors_allowed_origins=[
         "http://localhost:3000",
-        "https://moure.dev"
+        "https://moure.dev",
+        "https://primera-web-python-cunxzigxd-zw-arking-z.vercel.app/"
     ],
     plugins=[
         rx.plugins.SitemapPlugin(),
