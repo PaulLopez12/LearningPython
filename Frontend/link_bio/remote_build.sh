f5a1618608447bca75d55597be28f1a66394e92f
PaulLopez12 <67396684+PaulLopez12@users.noreply.github.com>
2026-09-25 18:24:30 -0500

    arreglado REFLEX_API_URL

--- Frontend/link_bio/remote_build.sh
+++ Frontend/link_bio/remote_build.sh
@@ -5,7 +5,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-REFLEX_SSR=false REFLEX_API_URL=https://primera-web-mouredev.up.railway.app/ reflex export --frontend-only
+REFLEX_SSR=false REFLEX_API_URL=https://primera-web-mouredev.up.railway.app reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
